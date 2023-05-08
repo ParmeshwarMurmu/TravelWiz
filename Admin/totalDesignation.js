fc739fff48d6449a65920f321bb5e16207a83f03
unknown <murmuparmeshwar05@gmail.com>
2023-05-08 09:59:57 +0530

    Day-5

--- Admin/totalDesignation.js
+++ Admin/totalDesignation.js
@@ -7,21 +7,32 @@ let pagination = document.querySelector(".pagination")
 let searchButton = document.getElementById("searchButton")
 let searchText = document.getElementById("searchText");
 
+let updatePriceDiv = document.getElementsByClassName("updatePriceDiv");
+let editPriceInput = document.getElementById("editPriceInput");
+let updateBtn = document.getElementById("updateBtn");
+
+
+
 let url = "http://localhost:3000/travel";
 
-window.addEventListener("load", ()=>{
-    calcLen()
-fetchingData(1);
-})
+// window.addEventListener("load", ()=>{
+//     calcLen()
+// fetchingData(1);
+// })
+calcLen()
+fetchingData();
 
-let len = 0;
+// let len = 0;
 
 function calcLen(){
     fetch(`${url}`)
-    .then((res)=> res.json())
+    .then((res)=>{
+        // len = res.headers.get("X-Total-Count")
+        return res.json();
+    })
     .then((data)=>{
         totalDesignation.innerText = data.length;
-        len = data.length;
+        // len = data.length;
     })
 }
 
@@ -29,7 +40,8 @@ function calcLen(){
 function fetchingData(pageNumber){
     fetch(`${url}?_limit=5&_page=${pageNumber}`)
     .then((res)=>{
-        let totalData = len;
+        console.log(res.headers.get("X-Total-Count"))
+        let totalData = res.headers.get("X-Total-Count")
         totalBtn = Math.ceil(totalData/5);
         pagination.innerHTML = "";
         for(let i=1; i<=totalBtn; i++)
@@ -39,10 +51,14 @@ function fetchingData(pageNumber){
         return res.json()
     })
     .then((data)=>{
-        console.group(data)
-        appendData(data)
+        // console.group(data)
+        appendData(data, pageNumber)
+        
     })
 }
+
+
+
 
 
 function getBtn(i){
@@ -63,31 +79,33 @@ function getBtn(i){
 
 
 
-function appendData(data)
+function appendData(data, pageNumber)
 {
+    console.log(data, "+++")
+    // data.rev
+    // console.log(data)
     desiBody.innerHTML = "";
     data.forEach(ele => {
-        desiBody.append(getCard(ele["image"], ele["placeDescription"], ele["price"], ele["category"], ele["city"]))
+        // console.log(ele);
+        desiBody.append(getCard(ele["image"], ele["placeDescription"], ele["price"], ele["category"], ele["city"], ele["id"], pageNumber))
     });
 }
 
-function getCard(image, placeDescription, price, category, city)
+function getCard(image, placeDescription, price, category, city, id, pageNumber)
 {
     let div = document.createElement("div");
     div.classList.add("card-body")
-    // let tr = document.createElement("tr");
-    
-    // let td1 = document.createElement("td");
+
     let imgDiv = document.createElement("div")
     imgDiv.classList.add("image-div");
 
     let img = document.createElement("img");
     img.setAttribute("src", image);
     imgDiv.append(img);
-    // td1.append(imgDiv)
+
     
     
-    // let td2 = document.createElement("td");
+
     let paraDiv = document.createElement("div")
     paraDiv.classList.add("paraDiv");
     let para = document.createElement("p")
@@ -95,35 +113,93 @@ function getCard(image, placeDescription, price, category, city)
     para.innerText = placeDescription;
     paraDiv.append(para)
 
-    // td2.append(para);
+
     
 
-    // let td3 = document.createElement("td");
+
     let cityHead = document.createElement("h2")
     cityHead.innerText = city;
-    // td3.append(cityHead);
+
 
     
-    // let td4 = document.createElement("td");
-    let btn = document.createElement("buton")
-    btn.classList.add("designationBtn");
-    btn.innerText = "Remove";
-    // td4.append(btn)
+
+    let removeBtn = document.createElement("buton")
+    removeBtn.classList.add("designationBtn");
+    removeBtn.innerText = "Remove";
+
+    removeBtn.addEventListener("click", ()=>{
+        removing(id, pageNumber)
+        // console.log("remove")
+    })
+
 
     let priceBtn = document.createElement("buton")
     priceBtn.classList.add("priceBtn");
     priceBtn.innerText = price;
 
-    // tr.append(td1, td2, td3, td4);
+  
 
     let editBtn = document.createElement("buton")
     editBtn.classList.add("editBtn");
     editBtn.innerText = "Edit Price";
 
-    div.append(imgDiv, cityHead, btn, priceBtn, editBtn, paraDiv)
+    editBtn.addEventListener("click", ()=>{
+        updatePriceDiv.style.display = "block"
+        
+    })
+
+    let updatePriceDiv = document.createElement("div");
+    updatePriceDiv.setAttribute("class", "updatePriceDiv")
+
+    let editPriceInput = document.createElement("input");
+    editPriceInput.setAttribute("type", "number")
+    editPriceInput.setAttribute("placeholder", "Enter Price")
+    editPriceInput.setAttribute("id", "editPriceInput");
+
+    let updateBtn = document.createElement("buton")
+    updateBtn.setAttribute("id", "updateBtn")
+    updateBtn.innerText = "Update";
+
+    updateBtn.addEventListener("click", ()=>{
+        // editingPrice();
+        // let price = editPriceInput.value
+        if(editPriceInput.value == "")
+        {
+            alert("Enter a valid value");
+        }
+        else{
+            let obj = {
+                price: editPriceInput.value
+                }
+        
+                fetch(`${url}/${id}`, {
+                    method: "PATCH",
+                    headers:{
+                        "Content-Type": "application/json"
+                    },
+                    body: JSON.stringify(obj)
+                })
+                .then((res)=> res.json())
+                .then((data)=>{
+                    calcLen()
+                    fetchingData(1);
+                })
+
+        }
+        
+    })
+
+    updatePriceDiv.style.display = "none";
+
+    updatePriceDiv.append(editPriceInput, updateBtn)
+
+    div.append(imgDiv, cityHead, removeBtn, priceBtn, editBtn,updatePriceDiv, paraDiv)
 
     return div;
 }
+
+let cardBody = document.getElementById("card-body");
+cardBody.classList.add("invisible1");
 
 
 searchButton.addEventListener("click", ()=>{
@@ -136,3 +212,24 @@ searchButton.addEventListener("click", ()=>{
         appendData(data)
     })
 })
+
+
+// removing
+
+function removing(id, pageNumber){
+    console.log("remove");
+    console.log("page", pageNumber)
+
+    fetch(`${url}/${id}`, {
+        method: "DELETE",
+        headers:{
+            "Content-Type": "application/json"
+        }
+    })
+    .then((res)=> res.json())
+    .then((data)=>{
+    //    calcLen()
+       fetchingData(pageNumber);
+    })
+
+}
